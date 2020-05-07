1a443a68b7bf71c9d8271454d65866716307f405
Alessandro Chitolina <alekitto@gmail.com>
2020-05-07 15:28:03 +0200

    initial commit. add auto submit request handler

---
+++ tests/Form/AutoSubmitRequestHandlerTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Solido\Common\Tests\Form;
+
+use Solido\Common\Form\AutoSubmitRequestHandler;
+use Symfony\Component\Form\RequestHandlerInterface;
+
+class AutoSubmitRequestHandlerTest extends HttpFoundationRequestHandlerTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRequestHandler(): RequestHandlerInterface
+    {
+        return new AutoSubmitRequestHandler($this->serverParams);
+    }
+
+    /**
+     * @dataProvider methodProvider
+     */
+    public function testDoNotSubmitFormWithEmptyNameIfNoFieldInRequest($method): void
+    {
+        self::markTestSkipped('Not applicable to this request handler');
+    }
+
+    /**
+     * @dataProvider methodProvider
+     */
+    public function testDoSubmitFormWithEmptyNameIfNoFieldInRequest(string $method): void
+    {
+        $form = $this->createForm('', $method, true);
+        $form->add($this->createForm('param1'));
+        $form->add($this->createForm('param2'));
+
+        $this->setRequestData($method, [
+            'paramx' => 'submitted value',
+        ]);
+
+        $this->requestHandler->handleRequest($form, $this->request);
+
+        self::assertTrue($form->isSubmitted());
+    }
+}
